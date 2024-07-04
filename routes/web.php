71a9a9f76d6b6f03c49672ab6a65bae8137c1623
gabrielcipriaano <devgabrielcipriano@gmail.com>
2024-07-04 14:45:24 -0600

    INITIAL COMMIT

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('main');
+});
+Route::get('/create-account', [RegisterController::class, 'index'])->name('register');
+Route::post('/create-account', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
